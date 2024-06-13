6ad8e42d91e7b82461496743fd2694244e062387
Kingcrusher <tiwarivikrant9764@gmail.com>
2024-06-13 17:13:25 +0530

    update api.views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render, HttpResponse
 from .models import Project, Tag, About
 from rest_framework import generics, status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
 from .serializers import ProjectSerializer, TagSerializer, AboutSerializer
 
 
 # Create your views here.
-class ProjectList(APIView):
-    def get(self, request, format=None):
-        projects = Project.objects.filter()
-        serializer = ProjectSerializer(projects, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+# class ProjectList(APIView):
+#     def get(self, request, format=None):
+#         projects = Project.objects.filter()
+#         serializer = ProjectSerializer(projects, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ProjectList(generics.ListAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
 
 
 class TagList(generics.ListAPIView):
