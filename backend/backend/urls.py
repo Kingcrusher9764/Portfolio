055f341efa4bb1ffe18a86f72cca92a143493aac
Kingcrusher <tiwarivikrant9764@gmail.com>
2024-06-13 16:47:28 +0530

    mdoified routes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,10 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from api.views import status
 
 urlpatterns = [
-    path("/", status, name="status"),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
